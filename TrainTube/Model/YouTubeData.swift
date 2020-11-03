664e61bd0118657a7261c288e3449c86606b30ac
Kumie-M <49703108+Kumie-M@users.noreply.github.com>
2020-11-03 22:32:42 +0900

    YouTubeData.swiftファイル追加

---
+++ TrainTube/Model/YouTubeData.swift
@@ -0,0 +1,15 @@
+//
+//  YouTubeData.swift
+//  TrainTube
+//
+//  Created by Kumie Miyata on 2020/11/03.
+//
+
+import Foundation
+
+class YouTubeData {
+    var videoId:String = ""
+    var title:String = ""
+    var imageURLString:String = ""
+    var youtubeURL:String = ""
+}
