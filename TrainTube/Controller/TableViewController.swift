96225d08b86c9318d1fd6ab972b33bc9da0471e8
Kumie-M <49703108+Kumie-M@users.noreply.github.com>
2020-11-03 22:49:22 +0900

    JSON解析の記述

--- TrainTube/Controller/TableViewController.swift
+++ TrainTube/Controller/TableViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class TableViewController: UITableViewController {
     
@@ -44,5 +47,40 @@ class TableViewController: UITableViewController {
         var text = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBNbCP8Ns3DKgKcXj4oDLY6X9eV-mTc5Kk&q=ふみきりチャンネル ふみきりアニメ 電車 新幹線 踏切&part=snippet&maxResults=40&order=date"
         
         let url = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        AF.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            print(response)
+            
+            switch response.result {
+            case .success:
+                
+                for i in 0...29 {
+                    
+                    let json:JSON = JSON(response.data as Any)
+                    var videoId = json["items"][i]["id"]["playlistId"].string
+                    
+                    if videoId == nil {
+                        videoId = json["items"][i]["id"]["videoId"].string
+                    }
+                    
+                    let title = json["items"][i]["snippet"]["title"].string
+                    let imageURLString = json["items"][i]["snippet"]["thumbnails"]["default"]["url"].string
+                    let youtubeURL = "https://www.youtube.com/watch?v=\(videoId!)"
+                    
+                    self.videoIdArray.append(videoId!)
+                    self.titleArray.append(title!)
+                    self.imageURLStringArray.append(imageURLString!)
+                    self.youtubeURLArray.append(youtubeURL)
+                }
+                break
+                
+            case .failure(let error):
+                print(error)
+                break
+            }
+            
+            self.tableView.reloadData()
+            
+        }
     }
 }
