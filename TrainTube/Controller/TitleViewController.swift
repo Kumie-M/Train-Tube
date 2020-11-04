ed76463ca236dffca964a54e56d4433c64e13abf
Kumie-M <49703108+Kumie-M@users.noreply.github.com>
2020-11-04 11:23:58 +0900

    UI変更・DispatchQueueの記述

--- TrainTube/Controller/TitleViewController.swift
+++ TrainTube/Controller/TitleViewController.swift
@@ -1,5 +1,5 @@
 //
-//  titleViewController.swift
+//  TitleViewController.swift
 //  TrainTube
 //
 //  Created by Kumie Miyata on 2020/11/03.
@@ -7,23 +7,17 @@
 
 import UIKit
 
-class titleViewController: UIViewController {
-
+class TitleViewController: UIViewController {
+    
+    var timer:Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            
+            let nextVC = TableViewController()
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
