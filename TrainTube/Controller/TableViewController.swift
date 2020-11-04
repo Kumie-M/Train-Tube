bf75dfc538d9082ca4efe968ddb4c92e09576624
Kumie-M <49703108+Kumie-M@users.noreply.github.com>
2020-11-04 10:15:12 +0900

    UI変更・TabeleViewControllerのViewDidLoad追記

--- TrainTube/Controller/TableViewController.swift
+++ TrainTube/Controller/TableViewController.swift
@@ -23,12 +23,8 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        getData()
     }
 
     @objc var scrollView: UIScrollView {
